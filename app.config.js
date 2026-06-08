77c9628a1d07bddd49c6bc5d6835ac26f6ab02af
Kaceme <kbadsi@gmail.com>
2026-06-08 14:54:09 +0200

    Securisation cle Google Maps via app.config.js et env

---
+++ app.config.js
@@ -0,0 +1,40 @@
+export default {
+  expo: {
+    name: "Movidia",
+    slug: "Movidia",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      },
+      package: "com.kaceme.Movidia"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "8a75ddf0-715f-4f21-b71b-c6d6d480cf02"
+      }
+    }
+  }
+};
